de062da915d5ae9af79b7ff720821ab4184fa23f
zaidanki <44997660+zaidanki@users.noreply.github.com>
2019-03-21 09:07:35 -0400

    Updates

---
+++ precache-manifest.bff583eb25bea0d2dbf0cf07c45b72a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8137127792096ab0fa10",
+    "url": "/react-deploy/static/css/main.3a14510f.chunk.css"
+  },
+  {
+    "revision": "8137127792096ab0fa10",
+    "url": "/react-deploy/static/js/main.81371277.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/react-deploy/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "00539649abe3b4db1174",
+    "url": "/react-deploy/static/js/runtime~main.00539649.js"
+  },
+  {
+    "revision": "e13573c3f3aa7eb220f1b08897becadc",
+    "url": "/react-deploy/index.html"
+  }
+];
